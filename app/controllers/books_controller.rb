2c8ba45755cbcad2bb42b665faf9e50156e27e04
EC2 Default User <ec2-user@ip-172-31-42-91.ec2.internal>
2022-06-09 15:13:47 +0000

    Initial Commit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
   end
 
-  def index
+  # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
-  def show
-  end
-
-  def edit
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'books/new'
   post 'books' => 'books#create'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'homes/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/top' => 'homes#top'
+  
 end

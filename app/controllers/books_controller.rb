5f9b14fbde2935ea0eb08f617a7ffbfa699ba17d
EC2 Default User <ec2-user@ip-172-31-42-91.ec2.internal>
2022-06-13 11:53:07 +0000

    Initial Commit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,19 +9,53 @@ class BooksController < ApplicationController
   end
   # 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end  
   end
+
     def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
+    def edit
+    @book = Book.find(params[:id])
+    
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+   
+
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+   def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+        @books = Book.all
+      render "index"
+    end
+   end
+
   private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
